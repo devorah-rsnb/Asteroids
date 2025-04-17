a27a16e5824d31a31b0bf9423181d0098c11094b
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2025-04-17 17:51:17 -0400

    New construction signature

--- asteroid.py
+++ asteroid.py
@@ -5,11 +5,11 @@ from circleshape import CircleShape
 from constants import ASTEROID_MIN_RADIUS, SPLIT_MIN_ANGLE, SPLIT_MAX_ANGLE
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x: int, y: int, radius: int, velocity: pg.Vector2):
+        super().__init__(x, y, radius, velocity)
     
-    def split(self):
-        self.kill()
+    def split(self) -> None:
+        self.kill() # Remove self from asteroids, updatable and drawable groups
         if self.radius <= ASTEROID_MIN_RADIUS:
             return 
         else:
@@ -17,7 +17,5 @@ class Asteroid(CircleShape):
             velocity1 = self.velocity.rotate(angle)
             velocity2 = self.velocity.rotate(-angle)
             new_r = self.radius - ASTEROID_MIN_RADIUS
-            A1 = Asteroid(self.position.x, self.position.y, new_r)
-            A1.velocity = velocity1 * 1.2
-            A2 = Asteroid(self.position.x, self.position.y, new_r)
-            A2.velocity = velocity2 * 1.2
+            A1 = Asteroid(self.position.x, self.position.y, new_r, velocity1 * 1.2)
+            A2 = Asteroid(self.position.x, self.position.y, new_r, velocity2 * 1.2)
