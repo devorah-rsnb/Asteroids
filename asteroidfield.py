ae7362d0693460941f84e6efb8c98d2086de5b24
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2025-04-18 09:30:04 -0400

    Remove magic numbers from edge object accession

--- asteroidfield.py
+++ asteroidfield.py
@@ -6,26 +6,22 @@ from constants import *
 
 class AsteroidField(pg.sprite.Sprite):
     edges = [
-        [
-            DIR_RIGHT,
-            lambda y: pg.Vector2(-ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
-        ],
-        [
-            DIR_LEFT,
-            lambda y: pg.Vector2(
-                SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT
-            ),
-        ],
-        [
-            DIR_UP,
-            lambda x: pg.Vector2(x * SCREEN_WIDTH, -ASTEROID_MAX_RADIUS),
-        ],
-        [
-            DIR_DOWN,
-            lambda x: pg.Vector2(
-                x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS
-            ),
-        ],
+        {
+            "direction" : DIR_RIGHT,
+            "spawn_location" : lambda y: pg.Vector2(-ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT)
+        },
+        {
+            "direction" : DIR_LEFT,
+            "spawn_location" : lambda y: pg.Vector2(SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT)
+        },
+        {
+            "direction" : DIR_UP,
+            "spawn_location" : lambda x: pg.Vector2(x * SCREEN_WIDTH, -ASTEROID_MAX_RADIUS)
+        },
+        {
+            "direction" : DIR_DOWN,
+            "spawn_location" : lambda x: pg.Vector2(x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS)
+        }
     ]
 
     def __init__(self):
@@ -40,8 +36,8 @@ class AsteroidField(pg.sprite.Sprite):
         # spawn a new asteroid at a random edge
         edge = choice(self.edges)
         speed = randint(40, 100)
-        velocity = edge[0] * speed
+        velocity = edge["direction"] * speed
         velocity = velocity.rotate(randint(-30, 30))
-        position = edge[1](uniform(0, 1))
+        position = edge["spawn_location"](uniform(0, 1))
         kind = randint(1, ASTEROID_KINDS)
         Asteroid(position.x, position.y, ASTEROID_MIN_RADIUS * kind, velocity)
