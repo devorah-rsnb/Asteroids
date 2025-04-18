18edb9181f54a72fa857afd077d8c885e1352726
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2025-04-18 09:52:48 -0400

    Gave units to constants

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 import pygame as pg
 
 
+# Pixels
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
@@ -10,25 +11,25 @@ DIR_DOWN = pg.Vector2(0,-1)
 DIR_RIGHT = pg.Vector2(1,0)
 DIR_LEFT = pg.Vector2(-1,0)
 
-# All objects are polygons, so their line width needs to be defined
-STROKE_WIDTH = 2 # mm?
+# All objects are polygons, define their line width 
+STROKE_WIDTH = 2 # Pixels, supposedly
 
 ASTEROID_MAX_SPEED = 40 # px/s
 ASTEROID_MIN_SPEED = 100 # px/s
-ASTEROID_MIN_RADIUS = 20
+ASTEROID_MIN_RADIUS = 20 # px
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
-ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS # px
 SPLIT_ACCELERATION = 1.2 # px/s/s
 SPAWN_DEFLECTION = 30 # degrees
 
-PLAYER_RADIUS = 20
-PLAYER_TURN_SPEED = 300
-PLAYER_SPEED = 200
+PLAYER_RADIUS = 20 # px
+PLAYER_TURN_SPEED = 300 # degrees/s
+PLAYER_SPEED = 200 # px/s
 
-SHOT_RADIUS = 5
-PLAYER_SHOOT_SPEED = 500
-PLAYER_SHOOT_COOLDOWN = 0.3
+SHOT_RADIUS = 5 # px
+PLAYER_SHOOT_SPEED = 500 # px/s
+PLAYER_SHOOT_COOLDOWN = 0.3 # shots/s
 
-SPLIT_MIN_ANGLE = 20
-SPLIT_MAX_ANGLE = 50
+SPLIT_MIN_ANGLE = 20 # degrees
+SPLIT_MAX_ANGLE = 50 # degrees
